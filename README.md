ca3a13ab49fe76acffeca287533d618beea4482a
Aditya Navphule <navphule.1@iitj.ac.in>
2022-08-28 12:05:27 +0530

    v2.0b1 created

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-# YouTube_Downloader
-Creating a YTDownloader GUI using terminal
+# IF 'pip' not installed, install pip.
+# make sure 'rich' package is installed
 
-## Execution
-run : bash ./main_downloader.sh
+
+# install pip and rich
+
+	1. arch : sudo pacman -S python-pip && pip install rich
+	2. debian : sudo apt install python3-pip && pip install rich
+	3. fedora : sudo dnf install python-pip && pip install rich
+
+
--- requirements.sh
+++ requirements.sh
@@ -1,33 +1,56 @@
 #!/bin/bash
 
 
-if [ $(command -v dialog) ]
+# checking for requirements.
+
+distro=$1
+verbose_mode=$2
+
+pip_install=0
+
+if [ $(command -v pip) ]
 then
+	pip_install=1
+fi
+
+if [ $verbose_mode -eq 1 ]
+then
+	echo "checking requirements.."
+fi
+
+sleep 0.67
+
+if [[ $(command -v yt-dlp) && $pip_install -eq 1 ]]
+then
+	if [ $verbose_mode -eq 1 ]
+	then
+		echo "requirements fullfilled"
+	fi
 	:
 else
-	echo "req package not installed, installing."
-	echo "++++++++++++++++"
-	echo "enter distro"
-	echo "1. Arch Based"
-	echo "2. Debian Based"
-	echo "3. Fedora Based"
-	echo "++++++++++++++++"
-
-	read distro
+	echo "installing required dependencies"
 
 	if [ $distro -eq 1 ]
 	then
-		sudo pacman -Syu
-		sudo pacman -Sy dialog
+		# arch
+		sudo pacman -S yt-dlp python3-pip
+		
+
 	elif [ $distro -eq 2 ]
 	then
-		sudo apt update && sudo apt install dialog
-	elif [ $distro -eq 3 ]
-	then
-		sudo dnf update && sudo dnf install dialog
+		# debian
+		sudo apt install yt-dlp python3-pip
+	else
+		# fedora
+		sudo dnf install yt-dlp python3-pip
 	fi
-
-	exit
 fi
+
+if [[ $verbose_mode -eq 1 && $pip_install -eq 1 ]]
+then
+	echo "installing required python packages"
+	pip install numpy rich
+fi
+exit
 
 
